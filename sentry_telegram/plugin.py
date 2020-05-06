4c2d1f78f13e89ec2e83b6f11f0f09aec546abc6
kdima <d.kovbasa.dev@gmail.com>
2020-05-06 18:45:50 +0300

    t

--- sentry_telegram/plugin.py
+++ sentry_telegram/plugin.py
@@ -108,7 +108,7 @@ class TelegramNotificationsPlugin(notify.NotificationPlugin):
             'message': event.message,
             'project_name': group.project.name,
             'url': group.get_absolute_url(),
-            'data': event,
+            'data': event.__dict__,
         }
 
         template = self.get_message_template(group.project)
@@ -143,7 +143,7 @@ class TelegramNotificationsPlugin(notify.NotificationPlugin):
         self.logger.debug('Response code: %s, content: %s' % (response.status_code, response.content))
 
     def notify_users(self, group, event, fail_silently=False, **kwargs):
-        self.logger.info('Received notification for event: %s' % event)
+        self.logger.info('Received notification for event: %s' % event.__dict__)
         receivers = self.get_receivers(group.project)
         self.logger.debug('for receivers: %s' % ', '.join(receivers or ()))
         payload = self.build_message(group, event)
