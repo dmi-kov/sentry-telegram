5d2124de7bbffa4ba52042f93d7ab4ce5ed73b02
kdima <d.kovbasa.dev@gmail.com>
2020-05-07 12:42:07 +0300

    g

--- sentry_telegram/plugin.py
+++ sentry_telegram/plugin.py
@@ -103,14 +103,17 @@ class TelegramNotificationsPlugin(notify.NotificationPlugin):
         the_tags = defaultdict(lambda: '[NA]')
         the_tags.update({k:v for k, v in event.tags})
         raw = event.get_raw_data()
-        extra = raw.get("extra", "no additional data provided")
+
+        self.logger.info('raw data: %s' % raw)
+
+        # extra = raw.get("extra", "no additional data provided")
         names = {
             'title': event.title,
             'tag': the_tags,
             'message': event.message,
             'project_name': group.project.name,
             'url': group.get_absolute_url(),
-            'extra': extra,
+            'extra': raw,
         }
 
         template = self.get_message_template(group.project)
@@ -145,7 +148,7 @@ class TelegramNotificationsPlugin(notify.NotificationPlugin):
         self.logger.debug('Response code: %s, content: %s' % (response.status_code, response.content))
 
     def notify_users(self, group, event, fail_silently=False, **kwargs):
-        self.logger.info('Received notification for event: %s' % event)
+        self.logger.debug('Received notification for event: %s' % event)
         receivers = self.get_receivers(group.project)
         self.logger.debug('for receivers: %s' % ', '.join(receivers or ()))
         payload = self.build_message(group, event)
